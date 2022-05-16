a3ce5ad69d2b402546580c480be9a34979e3dcbc
ASaidOguz <ahmetsaid2136@gmail.com>
2022-05-16 10:10:06 +0300

    Week 2 Assignment complete

--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -11,11 +11,41 @@ contract MerkleTree is Verifier {
 
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+       uint8 size=15;
+       hashes=new uint256[](size);
+       for (uint i =0;i<8;i++){
+           hashes[i]=0;
+       }
+       hashes[8]=PoseidonT3.poseidon([hashes[0],hashes[1]]);
+       hashes[9]=PoseidonT3.poseidon([hashes[2],hashes[3]]);
+       hashes[10]=PoseidonT3.poseidon([hashes[4],hashes[5]]);
+       hashes[11]=PoseidonT3.poseidon([hashes[6],hashes[7]]);
+       hashes[12]=PoseidonT3.poseidon([hashes[8],hashes[9]]);
+       hashes[13]=PoseidonT3.poseidon([hashes[10],hashes[11]]);
+       hashes[14]=PoseidonT3.poseidon([hashes[12],hashes[13]]);
+       root=hashes[14];
     }
-
-    function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
-        // [assignment] insert a hashed leaf into the Merkle tree
-    }
+      
+    function insertLeaf(uint256 hashedLeaf) public returns ( uint256) {
+         //[assignment] insert a hashed leaf into the Merkle tree
+    
+        for(uint i=0;i<8;i++){
+           if (hashes[i]==0){
+               hashes[i]=hashedLeaf;
+               i=8;
+               
+           }
+        }
+        
+       hashes[8]=PoseidonT3.poseidon([hashes[0],hashes[1]]);
+       hashes[9]=PoseidonT3.poseidon([hashes[2],hashes[3]]);
+       hashes[10]=PoseidonT3.poseidon([hashes[4],hashes[5]]);
+       hashes[11]=PoseidonT3.poseidon([hashes[6],hashes[7]]);
+       hashes[12]=PoseidonT3.poseidon([hashes[8],hashes[9]]);
+       hashes[13]=PoseidonT3.poseidon([hashes[10],hashes[11]]);
+       hashes[14]=PoseidonT3.poseidon([hashes[12],hashes[13]]);
+        return  root=hashes[14];
+     }
 
     function verify(
             uint[2] memory a,
@@ -23,7 +53,9 @@ contract MerkleTree is Verifier {
             uint[2] memory c,
             uint[1] memory input
         ) public view returns (bool) {
-
+              
+        return verifyProof(a, b, c, input)==true&&root==input[0];
+            
         // [assignment] verify an inclusion proof and check that the proof root matches current root
     }
 }
